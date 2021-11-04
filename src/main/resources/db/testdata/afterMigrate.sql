fef7bfd334df6e781ca748a8b649c6f851554166
Brendo Arcanjo <brendo.arcanjo@ufca.edu.br>
2021-11-04 10:13:19 -0300

    Organizing the properties

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_category (id, name) VALUES (1, 'Produtividade');
-INSERT INTO tb_category (id, name) VALUES (2, 'Jogo');
+INSERT INTO tb_category (id, name) VALUES (1, 'Produtividade') ON CONFLICT (id) DO NOTHING;
+INSERT INTO tb_category (id, name) VALUES (2, 'Jogo') ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (1, 'App para gestão de tempo', 'Florest', 16.00, 1);
-INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (2, 'App retrô do Super Mário', 'Super Mário', 1.00, 2);
+INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (1, 'App para gestão de tempo', 'Florest', 16.00, 1) ON CONFLICT (id_app) DO NOTHING;;
+INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (2, 'App retrô do Super Mário', 'Super Mário', 1.00, 2) ON CONFLICT (id_app) DO NOTHING;
