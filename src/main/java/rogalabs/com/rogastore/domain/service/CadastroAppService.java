61b19d68f69ab3a01e1c96dc3ae3e9d9c99dc283
Brendo Arcanjo <brendo.arcanjo@ufca.edu.br>
2021-11-04 10:51:22 -0300

    Adding service layer

---
+++ src/main/java/rogalabs/com/rogastore/domain/service/CadastroAppService.java
@@ -0,0 +1,41 @@
+package rogalabs.com.rogastore.domain.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import rogalabs.com.rogastore.domain.exception.AppNotFoundException;
+import rogalabs.com.rogastore.domain.model.App;
+import rogalabs.com.rogastore.domain.repository.AppRepository;
+
+@Service
+public class CadastroAppService {
+
+	@Autowired
+	private AppRepository appRepository;
+
+	@Transactional
+	public App save(App app) {
+		return appRepository.save(app);
+	}
+
+	@Transactional
+	public void delete(Long appId) {
+		try {
+
+			appRepository.deleteById(appId);
+			appRepository.flush();
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new AppNotFoundException(appId);
+		}
+	}
+
+	@Transactional
+	public App seekOrFail(Long appId) {
+		return appRepository.findById(appId).orElseThrow(() -> new AppNotFoundException(appId));
+	}
+
+}
---
+++ src/main/java/rogalabs/com/rogastore/domain/service/CadastroCategoryService.java
@@ -0,0 +1,48 @@
+package rogalabs.com.rogastore.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import rogalabs.com.rogastore.domain.exception.AppNotFoundException;
+import rogalabs.com.rogastore.domain.exception.CategoryNotFoundException;
+import rogalabs.com.rogastore.domain.exception.EntityInUseException;
+
+import rogalabs.com.rogastore.domain.model.Category;
+import rogalabs.com.rogastore.domain.repository.CategoryRepository;
+
+@Service
+public class CadastroCategoryService {
+
+	private static final String MSG_CATEGORY_IN_USE = "Code %d category cannot be removed as it is in use";
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Transactional
+	public Category save(Category category) {
+		return categoryRepository.save(category);
+	}
+
+	@Transactional
+	public void delete(Long categoryId) {
+		try {
+
+			categoryRepository.deleteById(categoryId);
+			categoryRepository.flush();
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new AppNotFoundException(categoryId);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new EntityInUseException(String.format(MSG_CATEGORY_IN_USE, categoryId));
+		}
+	}
+
+	@Transactional
+	public Category seekOrFail(Long categoryId) {
+		return categoryRepository.findById(categoryId).orElseThrow(() -> new CategoryNotFoundException(categoryId));
+	}
+}
