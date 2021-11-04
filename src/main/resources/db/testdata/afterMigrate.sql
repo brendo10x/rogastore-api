919ef828ddb972535332f6db42fe0ec1427e6baf
Brendo Arcanjo <brendo.arcanjo@ufca.edu.br>
2021-11-04 09:39:07 -0300

    Configuring the flyway

---
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (id, name) VALUES (1, 'Produtividade');
+INSERT INTO tb_category (id, name) VALUES (2, 'Jogo');
+
+INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (1, 'App para gestão de tempo', 'Florest', 16.00, 1);
+INSERT INTO tb_app (id_app, description, name, price, category_id) VALUES (2, 'App retrô do Super Mário', 'Super Mário', 1.00, 2);
