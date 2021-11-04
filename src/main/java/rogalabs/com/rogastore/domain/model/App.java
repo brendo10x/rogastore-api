a9090f5982ad121c4ef709341e8788495903861a
Brendo Arcanjo <brendo.arcanjo@ufca.edu.br>
2021-11-04 09:37:52 -0300

    Organizing the model

--- src/main/java/rogalabs/com/rogastore/domain/model/App.java
+++ src/main/java/rogalabs/com/rogastore/domain/model/App.java
@@ -2,31 +2,35 @@ package rogalabs.com.rogastore.domain.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
- 
- 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne; 
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
- 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Builder
 @Entity
-@Table(name = "tb_product")
-public class Product implements Serializable {
- 
+@Table(name = "tb_app") 
+public class App implements Serializable {
+	 
 	private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long Id;
+	private Long id;
 	
 	@Column(nullable = false)
 	private String name;
@@ -40,4 +44,5 @@ public class Product implements Serializable {
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Category category;
+
 }
