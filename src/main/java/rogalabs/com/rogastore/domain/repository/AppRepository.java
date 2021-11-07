fa2227f0703195bef3a9766836830ecea6c3af89
Brendo Arcanjo <brendo.arcanjo@ufca.edu.br>
2021-11-07 18:24:12 -0300

    Adding the AppRepository

--- src/main/java/rogalabs/com/rogastore/domain/repository/AppRepository.java
+++ src/main/java/rogalabs/com/rogastore/domain/repository/AppRepository.java
@@ -1,11 +1,15 @@
 package rogalabs.com.rogastore.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import rogalabs.com.rogastore.domain.model.App;
+import rogalabs.com.rogastore.domain.model.Category;
 
 @Repository
-public interface AppRepository extends JpaRepository<App, Long> {
+public interface AppRepository extends JpaRepository<App, Long>, JpaSpecificationExecutor<App>{
+	
+	App findTopByCategoryOrderByPriceAsc(Category category);	
 
 }
